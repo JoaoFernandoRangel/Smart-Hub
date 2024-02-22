09c4addca4e766830a5335ba08fb7bf28bedcfc4
Gabriel Cordeiro <61151121+gabcordeiro@users.noreply.github.com>
2024-02-22 18:54:43 -0300

    adicionado rotacao da garra

---
+++ Assets/GarraFollowBody.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarraFollowBody : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objetoARodar; // Referência ao objeto que você deseja seguir
+    [SerializeField]
+    private Transform objetoASeguir; // Referência ao objeto que você deseja seguir
+
+    [SerializeField]
+    private Vector3 offset; // Deslocamento (offset) a ser aplicado à posição
+
+    [SerializeField]
+    TrackingScript trackingScript;
+    [SerializeField]
+    float rotacaoTransformadaEscopo;
+
+    public float velocidadeDoLerp = 2;
+
+    private void Start()
+    {
+        trackingScript.GarraValueChanged += TrackingScript_GarraValueChanged;
+    }
+    private void Update()
+    {
+        // Verifica se a referência do objeto a seguir está configurada
+        if (objetoASeguir != null && objetoARodar != null)
+        {
+            // Obtém a posição atual do objeto a seguir com o offset
+            Vector3 posicaoAlvo = objetoASeguir.position + offset;
+
+            // Move este objeto em direção à posição alvo suavemente
+            transform.position = posicaoAlvo;
+
+            // Copia a rotação do objeto de referência
+            transform.rotation = objetoASeguir.rotation;
+
+            // Copia a rotação do objeto pai
+            Quaternion novaRotacao = objetoASeguir.rotation;
+
+            // Substitui a rotação em torno do eixo Y pelo valor de rotacaoTransformadaEscopo
+            novaRotacao *= Quaternion.Euler(0, rotacaoTransformadaEscopo, 0);
+
+            // Aplica a nova rotação ao objetoARodar
+            objetoARodar.transform.rotation = novaRotacao;
+        }
+    }
+
+
+    private void TrackingScript_GarraValueChanged(string arg1, string arg2, string arg3, string arg4)
+    {
+        float.TryParse(arg4, out float rotacaoTransformada);/*
+        if (rotacaoTransformada > 0)
+        {
+            rotacaoTransformada += 90;
+            print(rotacaoTransformada);
+        }
+        else if (rotacaoTransformada == 0)
+        {
+            rotacaoTransformada = 180;
+            print(rotacaoTransformada);
+        }
+        else if (rotacaoTransformada < 0)
+        {
+            rotacaoTransformada -= 90;
+            print(rotacaoTransformada);
+        }*/
+        rotacaoTransformadaEscopo = rotacaoTransformada;
+
+    }
+
+
+
+}
---
+++ Assets/GarraRotationController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using UnityEngine;
+
+public class GarraRotationController : MonoBehaviour
+{
+    [SerializeField]
+    TrackingScript trackingScript;
+    [SerializeField]
+    GameObject parteFinalDaGarra;
+    [SerializeField]
+    Animator animator; // Adicionei uma referência para o Animator
+
+    private void Start()
+    {
+        trackingScript.GarraValueChanged += TrackingScript_GarraValueChanged;
+    }
+
+    private void TrackingScript_GarraValueChanged(string arg1, string arg2, string arg3, string arg4)
+    {
+        float.TryParse(arg4, out float toma);
+        if (toma > 0)
+        {
+            toma += 90;
+            print(toma);
+        }
+        else if (toma == 0)
+        {
+            toma = 180;
+            print(toma);
+        }
+        else if (toma < 0)
+        {
+            toma -= 90;
+            print(toma);
+        }
+
+        // Desative o IK
+        //DesativarIK();
+        //MainThreadDispatcher.Instance.Enqueue(() => GirarGarra(toma));
+
+    }
+    /*
+    private void GirarGarra(float toma)
+    {
+        MainThreadDispatcher.Instance.Enqueue(() => parteFinalDaGarra.transform.Rotate(0, toma, 0));
+
+        // Aguarde um momento (você pode ajustar o tempo conforme necessário)
+        //StartCoroutine(ReativarIKAfterDelay(0.5f));
+    }
+
+    private void DesativarIK()
+    {
+        // Desative o IK ou faça qualquer manipulação necessária aqui
+        if (animator != null)
+        {
+            MainThreadDispatcher.Instance.Enqueue(() => animator.enabled = false);
+        }
+    }
+
+    private IEnumerator ReativarIKAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        // Reative o IK ou faça qualquer manipulação necessária aqui
+        if (animator != null)
+        {
+            MainThreadDispatcher.Instance.Enqueue(() => animator.enabled = true);
+        }
+    }*/
+}
